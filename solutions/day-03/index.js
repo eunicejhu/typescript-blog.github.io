457783980e103a1de6ca2e9a5238b387e8841d6e
Isabella Hu <eunicejhu@gmail.com>
2020-10-17 23:34:33 +0800

    Add solution for day3 (setup Eslint & Prettier)

---
+++ solutions/day-03/index.js
@@ -0,0 +1,9 @@
+const name = name => name.toUppercase()
+const GREETING = 'hello'
+
+const NUMBERS = [2, 1]
+const A = a => {
+  a
+}
+// eslint-disable-next-line no-console
+console.log()
---
+++ solutions/day-03/prettier.js
@@ -0,0 +1,7 @@
+/**@format */
+const arr = (...args) => {
+  // eslint-disable-next-line no-console
+  console.log('prettier', args.length)
+}
+
+arr("hello")
