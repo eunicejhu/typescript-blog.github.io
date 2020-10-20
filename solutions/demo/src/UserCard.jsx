5e56beb120c6b99b7d0ba4d72783ff9f0097c3bf
Isabella Hu <eunicejhu@gmail.com>
2020-10-20 12:57:41 +0800

    Add solution for Day7 Level2

--- solutions/demo/src/UserCard.jsx
+++ solutions/demo/src/UserCard.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 import Avatar from './images/zuoqinhu.jpg'
 const SKILLS = ["HTML", "CSS", "Sass", "JS", "React", "Redux", "Node", "MongoDB", "Webpack", "Git", "Typescript", "D3.js", "Design System", "TDD", "SCRUM", "Jest", "MYSQL"]
 const formatedSkills = SKILLS.map(skill => <li key={skill}>{skill}</li>)
-export const UserCard = () => <div className="usercard_wrapper">
-    <img src={Avatar} alt="isabella"></img>
-    <h3>ZUOQIN HU (Isabella)</h3>
-    <p>Senior Frontend Developer, Paris</p>
-    <ul>
-        {formatedSkills}
-    </ul>
-    <p>joined on August 30, 2020</p>
-</div>
+
+class UserCard extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <div className="usercard_wrapper">
+        <img src={Avatar} alt="isabella"></img>
+        <h3>ZUOQIN HU (Isabella)</h3>
+        <p>Senior Frontend Developer, Paris</p>
+        <ul>
+            {formatedSkills}
+        </ul>
+        <p>joined on August 30, 2020</p>
+    </div>
+    }
+}
+export  {UserCard} 
