b91b41c86171e55e52ffd25c2f31bfb98573d338
Isabella Hu <eunicejhu@gmail.com>
2020-10-30 09:49:16 +0800

    Polish ConfirmPasswordInput & test, extract out validation logic

--- solutions/demo/src/components/input/ConfirmPasswordInput.jsx
+++ solutions/demo/src/components/input/ConfirmPasswordInput.jsx
@@ -1,49 +1,28 @@
 import React, { useState } from "react";
-import { produce } from "immer";
 import { useImmer } from "use-immer";
 import PropTypes from "prop-types";
 
 import style from "./ConfirmPasswordInput.module.scss";
 
-const VALIDATE_OPTIONS = {
-  minLength: [6, "Length should be greater than 6"],
-  maxLength: [22, "Length should be less than 22"],
-  pattern: [
-    /.*[a-z]+.*[A-Z]+.*/g,
-    "Should contain at least one uppercase and one lowercase letter",
-  ],
-};
-
-// Password should be 6-22 length, at least one uppercase and one lowercase
-// customize validaty message
-
-const validate = (value, options, cb) => {
-  const { minLength, maxLength, pattern } = options;
-  if (value.length < minLength[0]) {
-    cb(minLength[1]);
-  } else if (value.length > maxLength[0]) {
-    cb(maxLength[1]);
-  } else if (!value.match(pattern[0])) {
-    cb(pattern[1]);
-  } else {
-    cb("");
-  }
-};
-
-/**
- *
- * @param {handleConfirm} props
- * initial value {pwd: "", error: true, handlePwd, handleError}
- *
- */
-
-export const ConfirmPasswordInputFC = (props) => {
-  const { handleConfirm } = props;
+const ConfirmPasswordInput = (props) => {
+  const { handleConfirm, validate } = props;
   const [pwd, setPwd] = useState("");
   const [confirmPwd, setConfirmPwd] = useState("");
+  const [error, setError] = useImmer({ pwd: "", confirmPwd: "" });
 
   const handleChangePwd = (e) => {
-    setPwd(e.target.value);
+    const { value } = e.target;
+    setPwd(value);
+
+    // clear confirm password and error
+    setConfirmPwd("");
+    setError((draft) => {
+      draft.confirmPwd = "";
+    });
+
+    setError((draft) => {
+      draft.pwd = validate(value);
+    });
   };
   const handleChangeConfirmPwd = (e) => {
     const { value } = e.target;
@@ -51,6 +30,10 @@ export const ConfirmPasswordInputFC = (props) => {
     setConfirmPwd(value);
     if (isIdentical) {
       handleConfirm(pwd);
+    } else {
+      setError((draft) => {
+        draft.confirmPwd = "Password is not identical";
+      });
     }
   };
 
@@ -66,10 +49,12 @@ export const ConfirmPasswordInputFC = (props) => {
           aria-label="Input Password"
           onChange={handleChangePwd}
           data-testid="password"
-          // className={!error.pwd ? "" : style.invalid}
+          className={!error.pwd ? "" : style.invalid}
           value={pwd}
         />
-        {/* <small className={style.error}>{error.pwd}</small> */}
+        <small data-testid="error_pwd" className={style.error}>
+          {error.pwd}
+        </small>
       </div>
       <div>
         <label htmlFor="password_confirmation">Confirm Password:</label>
@@ -81,100 +66,24 @@ export const ConfirmPasswordInputFC = (props) => {
           aria-label="confirm password"
           onChange={handleChangeConfirmPwd}
           data-testid="password_confirmation"
-          // className={!error.confirmPwd ? "" : style.invalid}
+          className={!error.confirmPwd ? "" : style.invalid}
           value={confirmPwd}
         />
-        {/* <small className={style.error}>{error.confirmPwd}</small> */}
+        <small data-testid="error_confirmPwd" className={style.error}>
+          {error.confirmPwd}
+        </small>
       </div>
     </>
   );
 };
 
-ConfirmPasswordInputFC.propTypes = {
+ConfirmPasswordInput.propTypes = {
   handleConfirm: PropTypes.func,
+  validate: PropTypes.func,
 };
-ConfirmPasswordInputFC.defaultProps = {
-  handleConfirm: () => {},
+ConfirmPasswordInput.defaultProps = {
+  handleConfirm: (value) => value,
+  validate: () => {},
 };
-
-class ConfirmPasswordInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pwd: "",
-      confirmPwd: "",
-      error: { pwd: "", confirmPwd: "" },
-    };
-  }
-
-  handlePwdInputChange = (e) => {
-    const { value } = e.target;
-    const errorCb = (message) => {
-      this.setState(
-        produce((draft) => {
-          draft.error = { pwd: message, confirmPwd: "" };
-          draft.pwd = value;
-          draft.confirmPwd = "";
-        })
-      );
-    };
-    validate(value, VALIDATE_OPTIONS, errorCb);
-  };
-
-  handleConfirmPwdInputChange = (e) => {
-    const { value } = e.target;
-    const { pwd } = this.state;
-    this.setState((prevState) => ({ ...prevState, confirmPwd: value }));
-    if (value !== pwd) {
-      this.setState(
-        produce((draft) => {
-          draft.error.confirmPwd = "Password is not identical";
-        })
-      );
-    } else {
-      this.setState(
-        produce((draft) => {
-          draft.error.confirmPwd = "";
-        })
-      );
-    }
-  };
-
-  render() {
-    const { pwd, confirmPwd, error } = this.state;
-    return (
-      <>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            required
-            aria-label="Input Password"
-            onChange={this.handlePwdInputChange}
-            className={!error.pwd ? "" : style.invalid}
-            value={pwd}
-          />
-          <small className={style.error}>{error.pwd}</small>
-        </div>
-        <div>
-          <label htmlFor="password_confirmation">Confirm Password:</label>
-          <input
-            type="password"
-            id="password_confirmation"
-            name="password_confirmation"
-            required
-            aria-label="confirm password"
-            onChange={this.handleConfirmPwdInputChange}
-            className={!error.confirmPwd ? "" : style.invalid}
-            value={confirmPwd}
-          />
-          <small className={style.error}>{error.confirmPwd}</small>
-        </div>
-      </>
-    );
-  }
-}
 
 export default ConfirmPasswordInput;
--- solutions/demo/src/components/input/ConfirmPasswordInput.test.jsx
+++ solutions/demo/src/components/input/ConfirmPasswordInput.test.jsx
@@ -6,7 +6,7 @@ import { fireEvent } from "@testing-library/react";
 import { getByTestId } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 
-import { ConfirmPasswordInputFC } from "./ConfirmPasswordInput";
+import ConfirmPasswordInput from "./ConfirmPasswordInput";
 
 let container;
 beforeEach(() => {
@@ -19,39 +19,84 @@ afterEach(() => {
   container = null;
 });
 
-it("validate value when input password", () => {
-  //   act(() => {
-  //     render(<ConfirmPasswordInput />, container);
-  //   });
-  //   const passwordInput = container.querySelector("#password");
-  //   act(() => {
-  //     passwordInput.dispatchEvent(new UIEvent("change", { bubbles: true }));
-  //   });
+it("validate password when input password,  ", () => {
+  const validate = jest.fn();
+
+  act(() => {
+    render(<ConfirmPasswordInput validate={validate} />, container);
+  });
+
+  const passwordInput = getByTestId(container, "password");
+
+  fireEvent.change(passwordInput, { target: { value: "******" } });
+
+  expect(validate).toHaveBeenCalledTimes(1);
 });
 
-it("cannot handle confirm if password is not identical", () => {
-  const handleConfirm = jest.fn();
+it("show error when password is invalid", () => {
+  const ERROR_MESSAGE = "Password is invalid";
+  const validate = jest.fn(() => ERROR_MESSAGE);
+
   act(() => {
-    render(<ConfirmPasswordInputFC handleConfirm={handleConfirm} />, container);
+    render(<ConfirmPasswordInput validate={validate} />, container);
   });
   const passwordInput = getByTestId(container, "password");
-  fireEvent.change(passwordInput, { target: { value: "123456" } });
-  const confirmPasswordInput = getByTestId(container, "password_confirmation");
-  fireEvent.change(confirmPasswordInput, { target: { value: "12345" } });
+  const error = getByTestId(container, "error_pwd");
 
+  fireEvent.change(passwordInput, { target: { value: "INVALID_PASSWORD" } });
+
+  expect(validate).toHaveBeenCalledTimes(1);
+  expect(error).toHaveTextContent(ERROR_MESSAGE);
+});
+
+it("Show error when confirmation is not identical to password", () => {
+  const ERROR = "Password is not identical";
+  const handleConfirm = jest.fn();
+
+  act(() => {
+    render(<ConfirmPasswordInput handleConfirm={handleConfirm} />, container);
+  });
+
+  const passwordInput = getByTestId(container, "password");
+  const confirmPasswordInput = getByTestId(container, "password_confirmation");
+  const error = getByTestId(container, "error_confirmPwd");
+
+  fireEvent.change(passwordInput, { target: { value: "123456" } });
+  fireEvent.change(confirmPasswordInput, { target: { value: "ddsfsd" } });
+
+  expect(error).toHaveTextContent(ERROR);
   expect(handleConfirm).toHaveBeenCalledTimes(0);
 });
-it("able to  handle confirm if password is identical", () => {
-  const handleConfirm = jest.fn();
+
+it("Clear confirmation and error when modify password", () => {
   act(() => {
-    render(<ConfirmPasswordInputFC handleConfirm={handleConfirm} />, container);
+    render(<ConfirmPasswordInput />, container);
   });
+
   const passwordInput = getByTestId(container, "password");
-  fireEvent.change(passwordInput, { target: { value: "123456" } });
   const confirmPasswordInput = getByTestId(container, "password_confirmation");
+  const errorConfirmPwd = getByTestId(container, "error_confirmPwd");
+
+  fireEvent.change(passwordInput, { target: { value: "123456" } });
+  fireEvent.change(confirmPasswordInput, { target: { value: "1234s" } });
+  fireEvent.change(passwordInput, { target: { value: "78888" } });
+
+  expect(confirmPasswordInput).toHaveValue("");
+  expect(errorConfirmPwd).toBeEmpty();
+});
+
+it("Confirm when confirmation is identical to password", () => {
+  const handleConfirm = jest.fn();
+
+  act(() => {
+    render(<ConfirmPasswordInput handleConfirm={handleConfirm} />, container);
+  });
+
+  const passwordInput = getByTestId(container, "password");
+  const confirmPasswordInput = getByTestId(container, "password_confirmation");
+
+  fireEvent.change(passwordInput, { target: { value: "123456" } });
   fireEvent.change(confirmPasswordInput, { target: { value: "123456" } });
 
   expect(handleConfirm).toHaveBeenCalledTimes(1);
 });
-it("reset confirm password to empty when modify password", () => {});
-it("report error when confirm password is not identical", () => {});
