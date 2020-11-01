328ceec7f945a8b315b0a00c7a5caca61056cd27
Isabella Hu <eunicejhu@gmail.com>
2020-11-01 15:34:03 +0800

    display loading when fetching data

--- solutions/demo/src/pages/Cats.jsx
+++ solutions/demo/src/pages/Cats.jsx
@@ -5,13 +5,16 @@ const { avgWeight, avgLifeSpan } = require("../utils/cats_utils");
 
 function Cats() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const url = "https://api.thecatapi.com/v1/breeds";
+      setIsLoading(true);
       try {
         const { data: cats } = await axios.get(url);
         setData(cats);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -19,18 +22,20 @@ function Cats() {
     fetchData();
   }, []);
 
-  const averageLifeSpan = data.length ? avgLifeSpan(data) : "...";
-  const avergageWeight = data.length ? avgWeight(data) : "...";
+  const averageLifeSpan = avgLifeSpan(data);
+  const avergageWeight = avgWeight(data);
   const FormatedData = () => {
     return data.map(({ name, id }) => <li key={id}>{name}</li>);
   };
   return (
     <>
       <p>
-        The avergage weigth is
-        {avergageWeight}
+        {isLoading
+          ? "Loading..."
+          : `The avergage weigth is
+        ${avergageWeight}
         The avergage life span is
-        {averageLifeSpan}
+        ${averageLifeSpan}`}
       </p>
       <ul>
         <FormatedData />
