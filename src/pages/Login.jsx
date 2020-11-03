b8490e334ed9462fb399e4605e907ad274d86d11
Isabella Hu <eunicejhu@gmail.com>
2020-11-03 15:32:58 +0800

    Add Login Component , style  and test

---
+++ src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import "../styles/Login.scss";
+
+export default function Login({ cb }) {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (identifier && password) cb();
+  };
+  return (
+    <div className="form-container">
+      <div className="backdrop">
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <label htmlFor="identifier">Identifier</label>
+            <input
+              id="identifier"
+              name="identifier"
+              type="text"
+              value={identifier}
+              onChange={({ target: { value } }) => {
+                setIdentifier(value);
+              }}
+              required
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={({ target: { value } }) => {
+                setPassword(value);
+              }}
+              required
+            />
+          </div>
+          <div className="field">
+            <input type="submit" value="Login" data-testid="submit" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  cb: PropTypes.func,
+};
+Login.defaultProps = {
+  cb: () => {},
+};
---
+++ src/test/Login.test.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Login from "../pages/Login";
+
+test("get ***@gmail.com when set ***@gmail.com to Identifier input", () => {
+  const { container } = render(<Login />);
+  const identifier = container.querySelector("#identifier");
+  fireEvent.change(identifier, { target: { value: "***@gmail.com" } });
+  expect(identifier.getAttribute("value")).toBe("***@gmail.com");
+});
+
+test("get ***** when set ***** to Password input", () => {
+  const { container } = render(<Login />);
+  const password = container.querySelector("#password");
+  fireEvent.change(password, { target: { value: "*****" } });
+  expect(password.value).toBe("*****");
+});
+
+test("Fail to submit when password is not filled", () => {
+  const onSubmit = jest.fn();
+  const { getByTestId, container } = render(<Login cb={onSubmit} />);
+  const identifier = container.querySelector("#identifier");
+  const submitBtn = getByTestId("submit");
+
+  fireEvent.change(identifier, { target: { value: "some identifier" } });
+  fireEvent.submit(submitBtn);
+  expect(onSubmit).toHaveBeenCalledTimes(0);
+});
+
+test("Fail to submit when identifier is not filled", () => {
+  const onSubmit = jest.fn();
+  const { getByTestId, container } = render(<Login cb={onSubmit} />);
+  const password = container.querySelector("[name=password]");
+  const submitBtn = getByTestId("submit");
+
+  fireEvent.change(password, { target: { value: "****" } });
+  fireEvent.submit(submitBtn);
+  expect(onSubmit).toHaveBeenCalledTimes(0);
+});
+
+test("Succeed to submit when identifier and password are both filled", () => {
+  const onSubmit = jest.fn();
+  const { getByTestId, container } = render(<Login cb={onSubmit} />);
+  const identifier = container.querySelector("#identifier");
+  const password = container.querySelector("[name=password]");
+  const submitBtn = getByTestId("submit");
+
+  fireEvent.change(identifier, { target: { value: "some identifier" } });
+  fireEvent.change(password, { target: { value: "****" } });
+  fireEvent.submit(submitBtn);
+  expect(onSubmit).toHaveBeenCalledTimes(1);
+});
