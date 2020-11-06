c4f1bd38ed185232490936fdf6b811c5c0dff234
Isabella Hu <eunicejhu@gmail.com>
2020-11-06 11:14:32 +0800

    add useLogout

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,15 +1,15 @@
 import { useHistory } from "react-router-dom";
-import {useCookies} from 'react-cookie'
+import { useCookies } from "react-cookie";
 
 export default function useLogin() {
   const history = useHistory();
-  const [, setCookie] = useCookies(['isLoggedIn']);
+  const [, setCookie] = useCookies(["isLoggedIn"]);
   const login = ({ identifier, password }) => {
     setTimeout(() => {
       // API call to login}
-      setCookie('isLoggedIn', true, {path:"/"})
-      // back to previous page
-      history.go(-1);
+      setCookie("isLoggedIn", true, { path: "/" });
+      // go to home page
+      history.replace("/");
     }, 1000);
   };
   return login;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import useLogin from "../hooks/useLogin";
+import history from "../helpers/history";
 import "../styles/Login.scss";
 
 export default function Login() {
@@ -12,39 +13,41 @@ export default function Login() {
     if (identifier && password) login({ identifier, password });
   };
   return (
-    <div className="form-container">
-      <div className="backdrop">
-        <form onSubmit={handleSubmit}>
-          <div className="field">
-            <label htmlFor="identifier">Identifier</label>
-            <input
-              id="identifier"
-              name="identifier"
-              type="text"
-              value={identifier}
-              onChange={({ target: { value } }) => {
-                setIdentifier(value);
-              }}
-              required
-            />
-          </div>
-          <div className="field">
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              value={password}
-              onChange={({ target: { value } }) => {
-                setPassword(value);
-              }}
-              required
-            />
-          </div>
-          <div className="field">
-            <input type="submit" value="Login" data-testid="submit" />
-          </div>
-        </form>
+    <div className="form-container-bg">
+      <div className="form-container">
+        <div className="backdrop">
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <label htmlFor="identifier">Identifier</label>
+              <input
+                id="identifier"
+                name="identifier"
+                type="text"
+                value={identifier}
+                onChange={({ target: { value } }) => {
+                  setIdentifier(value);
+                }}
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={password}
+                onChange={({ target: { value } }) => {
+                  setPassword(value);
+                }}
+                required
+              />
+            </div>
+            <div className="field">
+              <input type="submit" value="Login" data-testid="submit" />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
