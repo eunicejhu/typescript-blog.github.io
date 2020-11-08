c8cb8a0cb6edcbe143d5944bb599081370244a47
Isabella Hu <eunicejhu@gmail.com>
2020-11-09 07:48:54 +0800

    Add custom hook (useLogin) & test

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,16 +1,23 @@
 import { useHistory } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import axios from "axios";
+import { AUTH_STATUS } from "../reducers/authReducer";
 
-export default function useLogin() {
+const LOGIN_URL = "LOGIN_URL";
+
+export default function useLogin(dispatch) {
   const history = useHistory();
-  const [, setCookie] = useCookies(["isLoggedIn"]);
-  const login = () => {
-    setTimeout(() => {
-      // TODO: API call to login with identifier & password
-      setCookie("isLoggedIn", true, { path: "/" });
-      // go to home page
+  const [, setCookie] = useCookies(["auth"]);
+  const login = async (identifier, password) => {
+    try {
+      const result = await axios.post(LOGIN_URL, { identifier, password });
+      const { data } = result;
+      dispatch({ type: AUTH_STATUS.SUCCESS, payload: data });
+      setCookie("auth", data, { path: "/" });
       history.replace("/");
-    }, 1000);
+    } catch (error) {
+      dispatch({ type: AUTH_STATUS.ERROR, payload: error });
+    }
   };
-  return login;
+  return { login };
 }
---
+++ src/hooks/useLogin.test.js
@@ -0,0 +1,83 @@
+import { renderHook, cleanup } from "@testing-library/react-hooks";
+import { useCookies } from "react-cookie";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import useLogin from "./useLogin";
+import { AUTH_STATUS } from "../reducers/authReducer";
+
+jest.mock("react-cookie", () => {
+  return { useCookies: jest.fn() };
+});
+jest.mock("react-router-dom", () => {
+  return { useHistory: jest.fn() };
+});
+jest.mock("axios");
+
+const dispatch = jest.fn();
+
+describe("login success", () => {
+  const setCookie = jest.fn();
+  const replace = jest.fn();
+
+  beforeAll(() => {
+    axios.post.mockResolvedValue({
+      data: { id: 1, username: "isabella" },
+      status: 200,
+    });
+    useCookies.mockImplementation(() => [null, setCookie]);
+    useHistory.mockImplementation(() => ({ replace }));
+  });
+  beforeEach(async () => {
+    // renderHook, login
+    const { result, waitForNextUpdate } = renderHook(() => useLogin(dispatch));
+    result.current.login("isabella", "****");
+    try {
+      await waitForNextUpdate({ timeout: 100 });
+    } catch (error) {
+      expect(error.timeout).toBeTruthy();
+    }
+  });
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("Dispatch LOGIN_SUCCESS", async () => {
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "AUTH_SUCCESS",
+      payload: { id: 1, username: "isabella" },
+    });
+  });
+  test("Store user into Cookie", async () => {
+    expect(setCookie).toHaveBeenLastCalledWith(
+      "auth",
+      { id: 1, username: "isabella" },
+      { path: "/" }
+    );
+  });
+  test("Redirect to Home", () => {
+    expect(replace).toHaveBeenLastCalledWith("/");
+  });
+});
+
+describe("login failure", () => {
+  beforeAll(() => {
+    axios.post.mockRejectedValueOnce("login error");
+  });
+
+  afterAll(() => {
+    cleanup();
+  });
+  test("Dispatch LOGIN_FAILURE", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useLogin(dispatch));
+    result.current.login({ identifier: "isabella", password: "****" });
+    try {
+      await waitForNextUpdate({ timeout: 100 });
+    } catch (error) {
+      expect(error.timeout).toBeTruthy();
+      expect(dispatch).toHaveBeenLastCalledWith({
+        type: AUTH_STATUS.ERROR,
+        payload: "login error",
+      });
+    }
+  });
+});
