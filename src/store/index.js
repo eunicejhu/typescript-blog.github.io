389b4022022b1cd56238091ed8857437d80047fb
Isabella Hu <eunicejhu@gmail.com>
2020-11-13 13:40:36 +0800

    feat: setup redux store

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import postsReducer from "../features/posts/postsSlice";
+
+export default configureStore({
+  reducer: { posts: postsReducer },
+});
