b42501889ed4955e1f4441496d7c5494fcc42ccb
Isabella Hu <eunicejhu@gmail.com>
2020-11-14 15:48:51 +0800

    test: create an artifact (renderWithStore)

--- src/test/StoreWrapper.tsx
+++ src/test/StoreWrapper.tsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import store from "../store";
 
 type Props = { children: React.ReactNode };
+
+// weakness: we lose the flexibility to pass in initialState
 const StoreWrapper: React.FC<Props> = ({ children }) => (
   <Provider store={store}>{children}</Provider>
 );
---
+++ src/test/renderWithStore.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { createStore, combineReducers } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import postReducer from "../features/posts/postsSlice";
+
+const INITIAL_STATE = {
+  posts: [
+    { id: "1", title: "First test Post!", content: "test!" },
+    { id: "2", title: "Second test Post", content: "test" },
+  ],
+};
+
+const renderWithStore = (
+  ui,
+  {
+    initialState = INITIAL_STATE,
+    store = createStore(combineReducers({ posts: postReducer }), initialState),
+  } = {}
+) => {
+  const wrapper = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+  return render(ui, { wrapper });
+};
+
+export default renderWithStore;
