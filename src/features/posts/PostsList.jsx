be63a6d54a4f94619be3efd6e40a14936f82cfd6
Isabella Hu <eunicejhu@gmail.com>
2020-11-14 23:05:28 +0800

    feat: plug AddPostForm into PostsList & snapshot test

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import AddPostForm from "./AddPostForm.tsx";
 
 const PostsList = () => {
   const posts = useSelector((state) => state.posts);
@@ -8,15 +9,20 @@ const PostsList = () => {
     posts.map((post) => (
       <article className="post-excerpt" key={post.id}>
         <h3>{post.title}</h3>
-        <p className="post-content">{post.content}</p>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
       </article>
     ));
 
   return (
-    <section className="posts-list">
-      <h2>Posts</h2>
-      {posts && posts.length ? renderPosts : "No posts"}
-    </section>
+    <>
+      <section className="add-post-form">
+        <AddPostForm />
+      </section>
+      <section className="posts-list">
+        <h2>Posts</h2>
+        {posts && posts.length ? renderPosts : "No posts"}
+      </section>
+    </>
   );
 };
 
--- src/features/posts/PostsList.test.jsx
+++ src/features/posts/PostsList.test.jsx
@@ -1,35 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import StoreWrapper from "../../test/StoreWrapper.tsx";
+import renderer from "react-test-renderer";
+import StoreWrapper from "../../test/StoreWrapper";
 import PostsList from "./PostsList";
-import renderWithStore from "../../test/renderWithStore";
-
-jest.mock("react-redux", () => {
-  return {
-    useSelector: jest
-      .fn()
-      .mockImplementationOnce(jest.requireActual("react-redux").useSelector)
-      .mockImplementationOnce(() => [])
-      .mockImplementationOnce(() => undefined),
-    Provider: jest.requireActual("react-redux").Provider,
-  };
-});
+import renderWithStore, { INITIAL_STATE } from "../../test/renderWithStore";
 
 test("show initial postsList", () => {
-  const { container, getByText } = renderWithStore(<PostsList />);
+  const { container, getByText } = renderWithStore(<PostsList />, {
+    initialState: INITIAL_STATE,
+  });
   expect(getByText(/First test Post!/i)).toBeInTheDocument();
   expect(container.querySelectorAll(".post-excerpt").length).toBe(2);
 });
 
 test("show no posts when posts is []", () => {
-  const { getByText } = render(<PostsList />, {
-    wrapper: StoreWrapper,
+  const { getByText } = renderWithStore(<PostsList />, {
+    initialState: { posts: [] },
   });
   expect(getByText(/No Posts/i)).toBeInTheDocument();
 });
-test("show no posts when posts is undefined", () => {
-  const { getByText } = render(<PostsList />, {
-    wrapper: StoreWrapper,
-  });
-  expect(getByText(/No Posts/i)).toBeInTheDocument();
+
+test("render correctly", () => {
+  const tree = renderer
+    .create(
+      <StoreWrapper>
+        <PostsList />
+      </StoreWrapper>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
