b999a9f107d3cb85dec7226e7b7b88e3c1545302
Isabella Hu <eunicejhu@gmail.com>
2020-11-15 22:02:40 +0800

    feat: render users as dropdown (AddPostForm)

--- src/features/posts/AddPostForm.test.jsx
+++ src/features/posts/AddPostForm.test.jsx
@@ -4,9 +4,12 @@ import AddPostForm from "./AddPostForm.tsx";
 import store from "../../store/index.ts";
 
 import renderWithStore from "../../test/renderWithStore";
+import { INITIAL_STATE } from "../../test/mock_data";
 
 // option1: manually mock store
-jest.mock("../../store");
+jest.mock("../../store/index.ts");
+store.getState.mockImplementation(() => INITIAL_STATE);
+
 jest.mock("@reduxjs/toolkit", () => ({
   ...jest.requireActual("@reduxjs/toolkit"),
   nanoid: () => "3R8imJks0AjrbC9ueNf_s",
@@ -16,24 +19,27 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-test("submit AddPostForm after title and content provided", () => {
+test("type text in title and content input, select a user from the dropdown of users, click save post button to add a post", () => {
   const { getByTestId, getByRole } = renderWithStore(<AddPostForm />, {
     store,
   });
-  fireEvent.change(getByTestId("title"), { target: { value: "Title 1" } });
-  fireEvent.change(getByTestId("content"), { target: { value: "Content 1" } });
+  fireEvent.change(getByTestId("title"), { target: { value: "Title 3" } });
+  fireEvent.change(getByTestId("content"), { target: { value: "Content 3" } });
+  fireEvent.change(getByTestId("users"), { target: { value: "2" } });
   fireEvent.click(getByRole("button"));
   expect(store.dispatch).toHaveBeenCalledWith({
     type: "posts/postAdded",
     payload: {
       id: "3R8imJks0AjrbC9ueNf_s",
-      title: "Title 1",
-      content: "Content 1",
+      title: "Title 3",
+      content: "Content 3",
+      userId: "2",
     },
   });
 });
+
 // option2: use customRender
-test("Both title and content should be provided to enable the submission", () => {
+test("Both title, content and userId should be provided to enable the submission", () => {
   const { getByTestId, getByRole } = renderWithStore(<AddPostForm />, {
     store,
   });
@@ -44,6 +50,9 @@ test("Both title and content should be provided to enable the submission", () =>
   fireEvent.change(getByTestId("content"), {
     target: { value: "Content 1" },
   });
+  fireEvent.click(getByRole("button"));
+  expect(store.dispatch).toHaveBeenCalledTimes(0);
+  fireEvent.change(getByTestId("users"), { target: { value: "1" } });
   fireEvent.click(getByRole("button"));
   expect(store.dispatch).toHaveBeenCalledTimes(1);
 });
--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { postAdded } from "./postsSlice";
-
+import { State } from "../../store";
 const AddPostForm: React.FC = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
   const dispatch = useDispatch();
-  const handleSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+
+  const users = useSelector((state: State) => state.users);
+
+  const renderUsersOptions =
+    users &&
+    users.map((user) => (
+      <option key={user.id} value={user.id}>
+        {user.name}
+      </option>
+    ));
+  const onTitleChanged = (e: React.SyntheticEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+  const onContentChanged = (e: React.SyntheticEvent<HTMLTextAreaElement>) => {
+    setContent(e.currentTarget.value);
+  };
+  const onUserChanged = (e: React.SyntheticEvent<HTMLSelectElement>) => {
+    setUserId(e.currentTarget.value);
+  };
+
+  const onAddPostClicked = (e: React.SyntheticEvent<HTMLInputElement>) => {
     e.preventDefault();
-    if (title && content) {
-      dispatch(postAdded({ title, content }));
+    if (title && content && userId) {
+      dispatch(postAdded({ title, content, userId }));
       setTitle("");
       setContent("");
     }
   };
+
   return (
     <div className="add-post">
-      <form data-testid="form" onSubmit={handleSubmit}>
+      <form>
         <div className="field">
           <label htmlFor="title">Title</label>
           <input
@@ -25,7 +48,7 @@ const AddPostForm: React.FC = () => {
             name="title"
             width={20}
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={onTitleChanged}
           />
         </div>
         <div className="field">
@@ -35,11 +58,22 @@ const AddPostForm: React.FC = () => {
             data-testid="content"
             name="content"
             value={content}
-            onChange={(e) => setContent(e.target.value)}
+            onChange={onContentChanged}
           ></textarea>
         </div>
         <div className="field">
-          <input type="submit" value="Add Post" />
+          <select
+            data-testid="users"
+            value={userId}
+            name="users"
+            id="users"
+            onChange={onUserChanged}
+          >
+            {renderUsersOptions}
+          </select>
+        </div>
+        <div className="field">
+          <input type="submit" value="Add Post" onClick={onAddPostClicked} />
         </div>
       </form>
     </div>
