7569348e64f4c63a7667419481eba15a2e817640
Isabella Hu <eunicejhu@gmail.com>
2020-11-18 00:14:52 +0800

    test: ReactionButtons test & snapshot

---
+++ src/features/posts/ReactionButtons.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { fireEvent } from "@testing-library/react";
+import { useDispatch } from "react-redux";
+import ReactionButtons from "./ReactionButtons";
+import { INITIAL_STATE } from "../../test/mock_data";
+import renderWithStoreAndRouter from "../../test/renderWithStoreAndRouter";
+import { reactionAdded } from "./postsSlice";
+
+jest.mock("react-redux", () => ({
+  ...(jest.requireActual("react-redux") as {}),
+  useDispatch: jest.fn(),
+}));
+jest.mock("./postsSlice.ts", () => ({
+  ...(jest.requireActual("./postsSlice.ts") as {}),
+  reactionAdded: jest.fn(),
+}));
+
+test("render correctly", () => {
+  const { asFragment } = renderWithStoreAndRouter(
+    <ReactionButtons post={INITIAL_STATE.posts[0]} />
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("click reaction", async () => {
+  const dispatch = jest.fn();
+  (useDispatch as jest.Mock).mockReturnValue(dispatch);
+  const { container } = renderWithStoreAndRouter(
+    <ReactionButtons post={INITIAL_STATE.posts[0]} />
+  );
+  fireEvent.click(container.querySelector("[name=heart]") as Element);
+  expect(dispatch).toHaveBeenCalled();
+  expect(reactionAdded).toHaveBeenCalled();
+});
