38eaa5ea584f989c741dfd61edba729849c5b051
Isabella Hu <eunicejhu@gmail.com>
2020-11-18 21:57:18 +0800

    feat: add fetchUsers thunk
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,14 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { INITIAL_STATE } from "../../test/mock_data";
 
 export interface User {
   id: string;
   name: string;
 }
 
+export interface UsersState {
+  data: User[];
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+}
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  let res;
+  /**
+   * TODO: axios
+   */
+  const fetch = new Promise<User[]>((resolve) => {
+    setTimeout(() => {
+      return resolve(INITIAL_STATE.users.data);
+    }, 1000);
+  });
+  res = await fetch;
+  return res;
+});
+
+const initialState: UsersState = {
+  data: [],
+  status: "idle",
+  error: null,
+};
+
 const usersSlice = createSlice({
   name: "users",
-  initialState: [] as User[],
+  initialState,
   reducers: {},
+  extraReducers: {
+    [(fetchUsers.fulfilled as unknown) as string]: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      state.data = action.payload;
+    },
+  },
 });
 
 export default usersSlice.reducer;
