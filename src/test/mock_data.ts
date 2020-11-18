bc407159a9b0e283b4a118fe1ee88e4463727242
Isabella Hu <eunicejhu@gmail.com>
2020-11-18 12:28:49 +0800

    clean: mock data

--- src/test/mock_data.ts
+++ src/test/mock_data.ts
@@ -3,14 +3,6 @@ import { State } from "../store/index";
 
 export const NOW = "2020-11-15T16:16:08.493Z";
 export const TIME_AGO = "28 minutes";
-export const EMPTY_POST = {
-  id: "",
-  title: "",
-  content: "",
-  userId: "",
-  date: "",
-  reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
-};
 export const INITIAL_STATE: State = {
   posts: [
     {
