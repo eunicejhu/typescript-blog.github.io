6a3f5a966bc1817f48bbd3b63081c101520616ce
Isabella Hu <eunicejhu@gmail.com>
2020-11-18 21:23:02 +0800

    feat: show different UI content as fetchPosts progresses
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,31 +1,53 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useRouteMatch, Link } from "react-router-dom";
 import AddPostForm from "./AddPostForm";
-import { selectAllPosts } from "../../store";
+import { selectAllPosts, State } from "../../store";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
+import { fetchPosts } from "./postsSlice";
+
 const PostsList = () => {
   const { path } = useRouteMatch();
-  const posts = useSelector(selectAllPosts);
-  const sortedPosts =
-    posts && posts.slice().sort((a, b) => a.date.localeCompare(b.date));
-  const renderPosts =
-    sortedPosts &&
-    sortedPosts.map((post) => (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <PostAuthor userId={post.userId} />
-        <TimeAgo date={post.date} />
-        <p>
-          <Link to={`${path}posts/${post.id}`}>See more</Link>
-        </p>
-        <ReactionButtons post={post} />
-      </article>
-    ));
+  const dispatch = useDispatch();
+  const postsStatus = useSelector((state: State) => state.posts.status);
+  const postsError = useSelector((state: State) => state.posts.error);
 
+  let content;
+
+  useEffect(() => {
+    if (postsStatus === "idle") dispatch(fetchPosts());
+  }, [dispatch, postsStatus]);
+
+  const posts = useSelector(selectAllPosts);
+  const sortedPosts = posts
+    .slice()
+    .sort((a, b) => a.date.localeCompare(b.date));
+  const renderedPosts = sortedPosts.map((post) => (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <PostAuthor userId={post.userId} />
+      <TimeAgo date={post.date} />
+      <p>
+        <Link to={`${path}posts/${post.id}`}>See more</Link>
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  ));
+
+  if (postsStatus === "loading") {
+    content = <div>loading...</div>;
+  } else if (postsStatus === "succeeded") {
+    if (posts.length) {
+      content = renderedPosts;
+    } else {
+      content = <div>No post</div>;
+    }
+  } else if (postsError) {
+    content = <div>{postsError}</div>;
+  }
   return (
     <>
       <section className="add-post-form">
@@ -33,7 +55,7 @@ const PostsList = () => {
       </section>
       <section className="posts-list">
         <h2>Posts</h2>
-        {posts && posts.length ? renderPosts : "No posts"}
+        {content}
       </section>
     </>
   );
