c97db697ba61b737ba6d031926b7e945738920af
Isabella Hu <eunicejhu@gmail.com>
2020-11-21 01:54:02 +0800

    feat: clean api client

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 import { INITIAL_STATE, NOTIFICATIONS } from "../test/mock_data";
 import { nanoid } from "@reduxjs/toolkit";
 class Client {
