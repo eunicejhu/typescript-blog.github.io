2f2e085c9d4c4b5b0667d1a7c3264c90aed04ac7
Isabella Hu <eunicejhu@gmail.com>
2020-11-21 17:41:24 +0800

    test: refactor ReactionButtons test
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/features/posts/ReactionButtons.test.tsx
+++ src/features/posts/ReactionButtons.test.tsx
@@ -1,34 +1,76 @@
 import React from "react";
-import { fireEvent } from "@testing-library/react";
-import { useDispatch } from "react-redux";
+import { fireEvent, render } from "@testing-library/react";
+import { Provider } from "react-redux";
 import ReactionButtons from "./ReactionButtons";
+import { fetchPosts } from "./postsSlice";
+import store from "../../store";
 import { INITIAL_STATE } from "../../test/mock_data";
-import renderWithStoreAndRouter from "../../test/renderWithStoreAndRouter";
-import { addReaction } from "./postsSlice";
+import { unwrapResult } from "@reduxjs/toolkit";
 
-jest.mock("react-redux", () => ({
-  ...(jest.requireActual("react-redux") as {}),
-  useDispatch: jest.fn(),
-}));
-jest.mock("./postsSlice.ts", () => ({
-  ...(jest.requireActual("./postsSlice.ts") as {}),
-  addReaction: jest.fn(),
-}));
-
-test("render correctly", () => {
-  const { asFragment } = renderWithStoreAndRouter(
-    <ReactionButtons post={INITIAL_STATE.posts.data[0]} />
+test("render correctly", async () => {
+  const ui = (
+    <Provider store={store}>
+      <ReactionButtons post={INITIAL_STATE.posts.data[0]} />
+    </Provider>
   );
+  const { asFragment } = render(ui);
   expect(asFragment()).toMatchSnapshot();
 });
 
 test("click reaction", async () => {
-  const dispatch = jest.fn();
-  (useDispatch as jest.Mock).mockReturnValue(dispatch);
-  const { container } = renderWithStoreAndRouter(
-    <ReactionButtons post={INITIAL_STATE.posts.data[0]} />
+  const ui = (
+    <Provider store={store}>
+      <ReactionButtons post={INITIAL_STATE.posts.data[0]} />
+    </Provider>
   );
-  fireEvent.click(container.querySelector("[name=heart]") as Element);
-  expect(dispatch).toHaveBeenCalled();
-  expect(addReaction).toHaveBeenCalled();
+  const { container, findAllByText } = render(ui);
+  const ResultAction = await store.dispatch(fetchPosts());
+  try {
+    unwrapResult(ResultAction);
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    // wait for the update of state after dispatch fetchPosts
+    expect(store.getState().posts).toMatchInlineSnapshot(`
+      Object {
+        "data": Array [
+          Object {
+            "content": "test!",
+            "date": "2020-11-14T16:16:08.493Z",
+            "id": "1",
+            "reactions": Object {
+              "eyes": 0,
+              "heart": 4,
+              "hooray": 0,
+              "rocket": 0,
+              "thumbsUp": 0,
+            },
+            "title": "First test Post!",
+            "userId": "1",
+          },
+          Object {
+            "content": "test",
+            "date": "2020-11-10T16:16:08.493Z",
+            "id": "2",
+            "reactions": Object {
+              "eyes": 0,
+              "heart": 0,
+              "hooray": 0,
+              "rocket": 0,
+              "thumbsUp": 3,
+            },
+            "title": "Second test Post",
+            "userId": "0",
+          },
+        ],
+        "error": undefined,
+        "status": "succeeded",
+        "updatePostError": undefined,
+      }
+    `);
+    fireEvent.click(container.querySelector("[name=heart]") as Element);
+    // wait for the updated state after click
+    await findAllByText(/ /i);
+    expect(store.getState().posts.data[0].reactions["heart"]).toBe(5);
+  }
 });
