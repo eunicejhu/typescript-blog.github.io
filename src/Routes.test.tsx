2a93de5d06fa214913d4566b42dca968495b1c0c
Isabella Hu <eunicejhu@gmail.com>
2020-11-21 19:37:42 +0800

    test: refactor Routes test
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/Routes.test.tsx
+++ src/Routes.test.tsx
@@ -1,62 +1,43 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { createMemoryHistory } from "history";
 import Routes from "./Routes";
-import store from "./store/index";
 import { Provider } from "react-redux";
-import renderWithStoreAndRouter from "./test/renderWithStoreAndRouter";
-import BrowserRouterWrapper from "./test/BrowserRouterWrapper";
-import StoreWrapper from "./test/StoreWrapper";
+import store from "./store/index";
 
-// It provides 3 ways to wrap App with router for testing.
-
+const withMemoryRouterAndStore = (ui: React.ReactNode, route: string) => (
+  <MemoryRouter initialEntries={[route]} initialIndex={0}>
+    <Provider store={store}>{ui}</Provider>
+  </MemoryRouter>
+);
 // wrap it with Router providing history. in case we need history
 test("route / load PostList page", async () => {
-  const history = createMemoryHistory({ initialEntries: ["/inital"] });
-  history.push("/");
-  const { findByText } = render(
-    <MemoryRouter initialEntries={["/inital", "/"]} initialIndex={1}>
-      <StoreWrapper>
-        <Routes />
-      </StoreWrapper>
-    </MemoryRouter>
-  );
+  const ui = withMemoryRouterAndStore(<Routes />, "/");
+  const { findByText } = render(ui);
   expect(await findByText("Posts")).toBeInTheDocument();
 });
 
 test("route /posts/1 load SinglePostPage", () => {
-  renderWithStoreAndRouter(<Routes />, { route: "/posts/1" });
-  expect(screen.getByText(/First test Post!/i)).toBeInTheDocument();
+  const ui = withMemoryRouterAndStore(<Routes />, "/posts/1");
+  const { getByText } = render(ui);
+  expect(getByText(/No post found/i)).toBeInTheDocument();
 });
 
-test("route /posts/edit/1 load EditPostForm", () => {
-  renderWithStoreAndRouter(<Routes />, { route: "/editPost/1" });
-  expect(screen.getByText(/Edit Post/i)).toBeInTheDocument();
+test("route /posts/edit/1 load EditPostForm", async () => {
+  const ui = withMemoryRouterAndStore(<Routes />, "/editPost/1");
+  const { findByText } = render(ui);
+  expect(await findByText(/Edit Post/i)).toBeInTheDocument();
 });
-
-// in case we don't need history, we can use renderWithRouter boilerplate
-// test("route /cats load Cats page", () => {
-//   renderWithBrowserRouter(<Routes />, { route: "/cats" });
-//   expect(screen.getByText(/Cats/i)).toBeInTheDocument();
-// });
 
 test("route /notifications load NotificationsList", async () => {
-  const ui = (
-    <MemoryRouter initialEntries={["/notifications"]} initialIndex={0}>
-      <Provider store={store}>
-        <Routes />
-      </Provider>
-    </MemoryRouter>
-  );
+  const ui = withMemoryRouterAndStore(<Routes />, "/notifications");
   const { findByText } = render(ui);
   expect(await findByText(/Notifications List/i)).toBeInTheDocument();
 });
 
-// BrowserRouterWrapper expose window globals by default,
 test("load No Match when unknown route", () => {
-  window.history.pushState({}, "Unknown page", "/unknown-route");
-  render(<Routes />, { wrapper: BrowserRouterWrapper });
+  const ui = withMemoryRouterAndStore(<Routes />, "/unknown-route");
+  const { getByText } = render(ui);
 
-  expect(screen.getByText(/No match/i)).toBeInTheDocument();
+  expect(getByText(/No match/i)).toBeInTheDocument();
 });
