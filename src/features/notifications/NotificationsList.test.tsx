71fb01284177e6a7836d1bdc27966ebc5fc6e1d4
Isabella Hu <eunicejhu@gmail.com>
2020-11-21 10:19:48 +0800

    feat: show author in Notification & update test

--- src/features/notifications/NotificationsList.test.tsx
+++ src/features/notifications/NotificationsList.test.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Provider } from "react-redux";
 import NotificationsList from "./NotificationsList";
-import renderWithStore from "../../test/renderWithStore";
+import { render } from "@testing-library/react";
 import Client from "../../api/client";
 import { NOTIFICATIONS } from "../../test/mock_data";
+import store from "../../store/index";
+import { fetchUsers } from "../users/usersSlice";
 jest.mock("../../api/client");
 
 describe("NotificationsList test", () => {
-  it("render correctly", async () => {
+  beforeAll(() => {
+    store.dispatch(fetchUsers());
+  });
+  it.only("render correctly", async () => {
     Client.fetchAllNotifications = jest
       .fn()
       .mockResolvedValue({ data: NOTIFICATIONS });
-    const { getByText, findAllByText, container } = renderWithStore(
-      <NotificationsList />
+    const { getByText, findAllByText, container } = render(
+      <Provider store={store}>
+        <NotificationsList />
+      </Provider>
     );
     expect(getByText(/Loading/i)).toBeInTheDocument();
     await findAllByText(/ /i); // technique to render after update
--- src/features/notifications/NotificationsList.tsx
+++ src/features/notifications/NotificationsList.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectAllNotifications } from "../../store/selectors";
+import { selectAllNotifications, selectAllUsers } from "../../store/selectors";
 import { useAppDispatch } from "../../store/index";
 import { fetchAll } from "./notificationsSlice";
 import TimeAgo from "../posts/TimeAgo";
 import { unwrapResult } from "@reduxjs/toolkit";
 const NotificationsList: React.FC = () => {
+  const users = useSelector(selectAllUsers);
   const notifications = useSelector(selectAllNotifications);
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useAppDispatch();
@@ -25,28 +26,31 @@ const NotificationsList: React.FC = () => {
     };
     fetchData();
   }, [dispatch]);
-  const renderedNotifications = notifications.map((notification) => (
-    <article key={notification.id}>
-      <p>
-        <b>author.name</b>: <span>{notification.message}</span>
-      </p>
-      <p>
-        <TimeAgo date={notification.date}></TimeAgo>
-      </p>
-    </article>
-  ));
+  const renderedNotifications = notifications.map((notification) => {
+    const author = users.find((user) => user.id === notification.userId)?.name;
+    return (
+      <article key={notification.id}>
+        <p>
+          <b>{author}</b>: <span>{notification.message}</span>
+        </p>
+        <p>
+          <TimeAgo date={notification.date}></TimeAgo>
+        </p>
+      </article>
+    );
+  });
 
   if (isLoading) {
     content = <p>Loading...</p>;
   } else {
-    content = (
-      <div>
-        <h2>Notifications List</h2>
-        {renderedNotifications}
-      </div>
-    );
+    content = renderedNotifications;
   }
-  return <>{content}</>;
+  return (
+    <div>
+      <h2>Notifications List</h2>
+      {content}
+    </div>
+  );
 };
 
 export default NotificationsList;
