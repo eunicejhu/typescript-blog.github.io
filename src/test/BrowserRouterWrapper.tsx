76c99959087d46663545f4232ae627d4b974eb79
Isabella Hu <eunicejhu@gmail.com>
2020-11-21 19:47:22 +0800

    clean: test harness
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/test/BrowserRouterWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-
-type Props = { children?: React.ReactNode };
-const BrowserRouterWrapper: React.FC<Props> = ({ children }) => (
-  <BrowserRouter>{children}</BrowserRouter>
-);
-
-export default BrowserRouterWrapper;
