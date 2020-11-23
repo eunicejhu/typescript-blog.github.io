67fa19cebee24fddec871bf7197ee8732058b635
Isabella Hu <eunicejhu@gmail.com>
2020-11-23 18:51:56 +0800

    test: refactor Routes test using miragejs
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/Routes.test.tsx
+++ src/Routes.test.tsx
@@ -1,43 +1,62 @@
 import React from "react";
 import { render } from "@testing-library/react";
+// eslint-disable-next-line testing-library/no-dom-import
+import { waitFor } from "@testing-library/dom";
 import { MemoryRouter } from "react-router-dom";
 import Routes from "./Routes";
 import { Provider } from "react-redux";
 import store from "./store/index";
 
-const withMemoryRouterAndStore = (ui: React.ReactNode, route: string) => (
-  <MemoryRouter initialEntries={[route]} initialIndex={0}>
-    <Provider store={store}>{ui}</Provider>
-  </MemoryRouter>
-);
-// wrap it with Router providing history. in case we need history
-test("route / load PostList page", async () => {
-  const ui = withMemoryRouterAndStore(<Routes />, "/");
-  const { findByText } = render(ui);
-  expect(await findByText("Posts")).toBeInTheDocument();
-});
+import { makeServer } from "./server";
+import { Server } from "miragejs";
+import { fetchPosts } from "./features/posts/postsSlice";
 
-test("route /posts/1 load SinglePostPage", () => {
-  const ui = withMemoryRouterAndStore(<Routes />, "/posts/1");
-  const { getByText } = render(ui);
-  expect(getByText(/No post found/i)).toBeInTheDocument();
-});
+describe("Routes test", () => {
+  let server: Server;
+  beforeEach(() => {
+    server = makeServer();
+  });
+  afterEach(() => {
+    server.shutdown();
+  });
+  const withMemoryRouterAndStore = (ui: React.ReactNode, route: string) => (
+    <MemoryRouter initialEntries={[route]} initialIndex={0}>
+      <Provider store={store}>{ui}</Provider>
+    </MemoryRouter>
+  );
+  // wrap it with Router providing history. in case we need history
+  test("route / load PostList page", async () => {
+    const ui = withMemoryRouterAndStore(<Routes />, "/");
+    const { findByText } = render(ui);
+    expect(await findByText("Posts")).toBeInTheDocument();
+  });
 
-test("route /posts/edit/1 load EditPostForm", async () => {
-  const ui = withMemoryRouterAndStore(<Routes />, "/editPost/1");
-  const { findByText } = render(ui);
-  expect(await findByText(/Edit Post/i)).toBeInTheDocument();
-});
+  test("route /posts/1 load SinglePostPage", () => {
+    const ui = withMemoryRouterAndStore(<Routes />, "/posts/1");
+    const { getByText } = render(ui);
+    expect(getByText(/No post found/i)).toBeInTheDocument();
+  });
 
-test("route /notifications load NotificationsList", async () => {
-  const ui = withMemoryRouterAndStore(<Routes />, "/notifications");
-  const { findByText } = render(ui);
-  expect(await findByText(/Notifications List/i)).toBeInTheDocument();
-});
+  it("route /posts/edit/1 load EditPostForm", async () => {
+    server.createList("post", 2);
+    store.dispatch(fetchPosts());
+    const ui = withMemoryRouterAndStore(<Routes />, "/editPost/1");
+    const { findByText } = render(ui);
+    expect(await findByText(/Edit Post/i)).toBeInTheDocument();
+  });
 
-test("load No Match when unknown route", () => {
-  const ui = withMemoryRouterAndStore(<Routes />, "/unknown-route");
-  const { getByText } = render(ui);
+  test("route /notifications load NotificationsList", async () => {
+    const ui = withMemoryRouterAndStore(<Routes />, "/notifications");
+    const { getByText } = render(ui);
+    await waitFor(() => {
+      expect(getByText(/Notifications List/i)).toBeInTheDocument();
+    });
+  });
 
-  expect(getByText(/No match/i)).toBeInTheDocument();
+  test("load No Match when unknown route", () => {
+    const ui = withMemoryRouterAndStore(<Routes />, "/unknown-route");
+    const { getByText } = render(ui);
+
+    expect(getByText(/No match/i)).toBeInTheDocument();
+  });
 });
