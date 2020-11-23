fbcea18ef94cd7c5e9fac17ea7f841b4bd4de118
Isabella Hu <eunicejhu@gmail.com>
2020-11-23 17:07:55 +0800

    test: refactor ReactionButtons test
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/features/posts/ReactionButtons.test.tsx
+++ src/features/posts/ReactionButtons.test.tsx
@@ -1,76 +1,66 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react";
+// eslint-disable-next-line testing-library/no-dom-import
+import { waitFor } from "@testing-library/dom";
 import { Provider } from "react-redux";
 import ReactionButtons from "./ReactionButtons";
 import { fetchPosts } from "./postsSlice";
 import store from "../../store";
-import { INITIAL_STATE } from "../../test/mock_data";
-import { unwrapResult } from "@reduxjs/toolkit";
 
-test("render correctly", async () => {
-  const ui = (
-    <Provider store={store}>
-      <ReactionButtons post={INITIAL_STATE.posts.data[0]} />
-    </Provider>
-  );
-  const { asFragment } = render(ui);
-  expect(asFragment()).toMatchSnapshot();
-});
+import { makeServer } from "../../server";
+import { Server } from "miragejs";
+let server: Server;
+describe("ReactionButtons test", () => {
+  beforeEach(() => {
+    server = makeServer();
+    server.createList("post", 2);
+    store.dispatch(fetchPosts());
+  });
+  afterEach(() => {
+    server.shutdown();
+  });
+  it("render correctly", async () => {
+    await waitFor(() => {
+      expect(store.getState().posts.data.length).toBe(2);
+    });
+    const post = store.getState().posts.data[0];
+    const ui = (
+      <Provider store={store}>
+        <ReactionButtons post={post} />
+      </Provider>
+    );
+    const { asFragment } = render(ui);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-test("click reaction", async () => {
-  const ui = (
-    <Provider store={store}>
-      <ReactionButtons post={INITIAL_STATE.posts.data[0]} />
-    </Provider>
-  );
-  const { container, findAllByText } = render(ui);
-  const ResultAction = await store.dispatch(fetchPosts());
-  try {
-    unwrapResult(ResultAction);
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    // wait for the update of state after dispatch fetchPosts
-    expect(store.getState().posts).toMatchInlineSnapshot(`
-      Object {
-        "data": Array [
-          Object {
-            "content": "test!",
-            "date": "2020-11-14T16:16:08.493Z",
-            "id": "1",
-            "reactions": Object {
-              "eyes": 0,
-              "heart": 4,
-              "hooray": 0,
-              "rocket": 0,
-              "thumbsUp": 0,
-            },
-            "title": "First test Post!",
-            "userId": "1",
-          },
-          Object {
-            "content": "test",
-            "date": "2020-11-10T16:16:08.493Z",
-            "id": "2",
-            "reactions": Object {
-              "eyes": 0,
-              "heart": 0,
-              "hooray": 0,
-              "rocket": 0,
-              "thumbsUp": 3,
-            },
-            "title": "Second test Post",
-            "userId": "0",
-          },
-        ],
-        "error": undefined,
-        "status": "succeeded",
-        "updatePostError": undefined,
-      }
-    `);
-    fireEvent.click(container.querySelector("[name=heart]") as Element);
-    // wait for the updated state after click
-    await findAllByText(/ /i);
-    expect(store.getState().posts.data[0].reactions["heart"]).toBe(5);
-  }
+  it("click reaction", async () => {
+    await waitFor(() => {
+      expect(store.getState().posts.data.length).toBe(2);
+    });
+    let post;
+    post = store.getState().posts.data[0];
+    const ui = (
+      <Provider store={store}>
+        <ReactionButtons post={post} />
+      </Provider>
+    );
+    const { getByTestId, rerender } = render(ui);
+    const heartButton = getByTestId("heart");
+
+    fireEvent.click(heartButton);
+    await waitFor(() => {
+      expect(store.getState().posts.data[0].reactions["heart"]).toBe(5);
+    });
+
+    // get updated post from store
+    post = store.getState().posts.data[0];
+    rerender(
+      <Provider store={store}>
+        <ReactionButtons post={post} />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(heartButton).toHaveTextContent(/5/i);
+    });
+  });
 });
