27607f5e1d9b2e509319c715c7cbe08b0467d024
Isabella Hu <eunicejhu@gmail.com>
2020-11-23 18:52:24 +0800

    test: small fix in PostsList test
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/features/posts/PostsList.test.tsx
+++ src/features/posts/PostsList.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import PostsList from "./PostsList";
 import { MemoryRouter, Route, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
