b3f712ad95285483a86413b397564318e6b02d7e
Isabella Hu <eunicejhu@gmail.com>
2020-11-26 22:55:30 +0800

    feature: add Button component & test, stories
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/components/buttons/Button.test.tsx
+++ src/components/buttons/Button.test.tsx
@@ -7,23 +7,45 @@ import { render } from "@testing-library/react";
 import "jest-styled-components";
 
 it("Button test:", () => {
-  const { container } = render(<Button theme={themes.pink} />);
-  expect(container.firstChild).toMatchInlineSnapshot(`
-    .c0 {
-      --color-default: #f8bbd0;
-      --color-light: #ffeeff;
-      --color-dark: #c48b9f  color:white;
-      background-color: var(--color-light);
-      border: 1px solid var(--color-light);
-    }
+    const { container } = render(<Button themes={themes} mode={"pink"} />);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+        .c0 {
+          --btn-text-hover: rgba(255,255,255,0.8);
+          --btn-bg-hover: rgba(196,139,159,0.8);
+          --color-disabled: rgba(196,139,159,0.4);
+          --color-disabled-bg: rgba(196,139,159,0.2);
+          --color-bg: rgba(255,255,255,1);
+          --color-text: rgba(196,139,159,1);
+          --color-border: rgba(196,139,159,1);
+          --border-radius: 4px;
+          --transition: all 0.2s;
+          --spacing: 10px;
+          color: var(--color-text);
+          background-color: var(--color-bg);
+          border: 1px solid var(--color-border);
+          border-radius: var(--border-radius);
+          -webkit-transition: var(--transition);
+          transition: var(--transition);
+          padding: var(--spacing);
+        }
 
-    .c0:hover {
-      background-color: var(--color-dark);
-      border: 1px solid var(--color-dark);
-    }
+        .c0:hover {
+          color: var(btn-text-hover);
+          cursor: pointer;
+          background-color: var(btn-bg-hover);
+          border: 1px solid var(btn-text-hover);
+        }
 
-    <button
-      class="c0"
-    />
-  `);
+        .c0:disabled {
+          color: var(--color-disabled);
+          cursor: not-allowed;
+          background-color: var(--color-disabled-bg);
+          border: 1px solid var(--color-disabled);
+        }
+
+        <button
+          class="c0"
+          mode="pink"
+        />
+    `);
 });
--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,31 +1,53 @@
 import React from "react";
 import styled from "styled-components";
+import { Themes, Mode } from "../../context/ThemeContext";
 
-const StyledButton = styled.button<{ variant: string }>`
-  ${(props) => {
-    return `--color-default:  ${props.theme.color[props.variant].default};
-  --color-light: ${props.theme.color[props.variant].light} ;
-  --color-dark: ${props.theme.color[props.variant].dark} `;
-  }}
+const StyledButton = styled.button<ButtonProps>`
+    ${({ themes, mode }: ButtonProps) => {
+        return `
+            --btn-text-hover:  ${themes[mode].btnTextHover};
+            --btn-bg-hover: ${themes[mode].btnBackgroundHover};
+            --color-disabled: ${themes[mode].disabled} ;
+            --color-disabled-bg: ${themes[mode].disabledBackground} ;
+            --color-bg: ${themes[mode].background} ;
+            --color-text:${themes[mode].text};
+            --color-border: ${themes[mode].btnBorder} ;
+            --border-radius: ${themes[mode].borderRadius};
+            --transition: ${themes[mode].transition};
+            --spacing: ${themes[mode].spacing};
+        `;
+    }}
+    color: var(--color-text);
+    background-color: var(--color-bg);
+    border: 1px solid var(--color-border);
+    border-radius: var(--border-radius);
+    transition: var(--transition);
+    padding: var(--spacing);
 
-  color: white;
-  background-color: var(--color-light);
-  border: 1px solid var(--color-light);
+    &:hover {
+        color: var(--btn-text-hover);
+        cursor: pointer;
+        background-color: var(--btn-bg-hover);
+        border: 1px solid var(--btn-text-hover);
+    }
 
-  &:hover {
-    background-color: var(--color-dark);
-    border: 1px solid var(--color-dark);
-  }
+    &:disabled {
+        color: var(--color-disabled);
+        cursor: not-allowed;
+        background-color: var(--color-disabled-bg);
+        border: 1px solid var(--color-disabled);
+    }
 `;
 
-const Button: React.FC<{ children?: React.ReactNode; [p: string]: any }> = ({
-  children,
-  variant = "primary",
-  ...props
-}) => (
-  <StyledButton {...props} variant={variant}>
-    {children}
-  </StyledButton>
+export interface ButtonProps {
+    children?: React.ReactNode;
+    themes: Themes;
+    mode: Mode;
+    [p: string]: any;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => (
+    <StyledButton {...props}>{children}</StyledButton>
 );
 
 export default Button;
