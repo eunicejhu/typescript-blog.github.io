ea15d3787bd29546f03cc954929a031d6d5061b8
Isabella Hu <eunicejhu@gmail.com>
2020-11-26 22:54:31 +0800

    feat: add themes in ThemeContext, add useTheme
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,45 +1,61 @@
 import React from "react";
 
+export const LIGHT = {
+    text: "rgba(0, 0, 0, 1)",
+    background: "rgba(255, 255, 255, 1)",
+    btnBackground: "transparent",
+    btnBorder: "rgba(0, 0, 0, 1)",
+    btnTextHover: "rgba(255, 255, 255, 0.8)",
+    btnBackgroundHover: "rgba(0, 0, 0, 0.8)",
+    disabled: "rgba(0, 0, 0, 0.24)",
+    disabledBackground: "rgba(255, 255, 255, 0.2)",
+};
+export const DARK = {
+    text: "rgba(255, 255, 255, 1)",
+    background: "rgba(0, 0, 0, 1)",
+    btnBackground: "transparent",
+    btnBorder: "rgba(255, 255, 255, 1)",
+    btnTextHover: "rgba(0, 0, 0, 0.8)",
+    btnBackgroundHover: "rgba(255, 255, 255, 0.8)",
+    disabled: "rgba(255, 255, 255, 0.4)",
+    disabledBackground: "rgba(0, 0, 0, 0.2)",
+};
+
 const PINK = {
-  color: {
-    primary: {
-      light: "#ffeeff",
-      default: "#f8bbd0",
-      dark: "#c48b9f",
-    },
-    secondary: {
-      light: "#ffffff",
-      default: "#fce4ec",
-      dark: "#c9b2ba",
-    },
-    text: {
-      primary: "#3e2723",
-      secondary: "#5d4037",
-    },
-    bg: "#e1e2e1",
-  },
-  borderRadius: "4px",
+    text: "rgba(196, 139, 159, 1)",
+    background: "rgba(255, 255, 255, 1)",
+    btnBackground: "transparent",
+    btnBorder: "rgba(196, 139, 159, 1)",
+    btnTextHover: "rgba(255, 255, 255, 0.8)",
+    btnBackgroundHover: "rgba(196, 139, 159, 0.8)",
+    disabled: "rgba(196, 139, 159, 0.4)",
+    disabledBackground: "rgba(196, 139, 159, 0.2)",
 };
 
-const DARK = {
-  /**TODO */
+const OTHER = {
+    borderRadius: "4px",
+    transition: "all 0.2s",
+    spacing: "10px",
 };
 
-const LIGHT = {
-  /**TODO */
-};
 export const themes = {
-  dark: DARK,
-  light: LIGHT,
-  pink: PINK,
+    dark: { ...DARK, ...OTHER },
+    light: { ...LIGHT, ...OTHER },
+    pink: { ...PINK, ...OTHER },
 };
 
-const ThemeContext = React.createContext<{ theme: typeof PINK; setTheme: any }>(
-  {
-    theme: themes.pink,
+export type Mode = "pink" | "dark" | "light";
+export type Themes = typeof themes;
+
+const ThemeContext = React.createContext<{
+    mode: Mode;
+    themes: Themes;
+    setTheme: any;
+}>({
+    mode: "pink",
+    themes: themes,
     setTheme: () => {},
-  }
-);
+});
 
 ThemeContext.displayName = "ThemeContext";
 
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
+
+export default function useTheme() {
+    const theme = useContext(ThemeContext);
+    return theme || {};
+}
